8c6d3b66ef5d977a2fe70e44ec89db4e1a8739ce
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-10-27 16:18:23 +0900

    ㅇ
    ㅇ

--- 토스/js/jquery.js
+++ 토스/js/jquery.js
@@ -1,4 +1,14 @@
 $(function(){
 
+  $(window).on("scroll",function(){ // 헤더색상추가
+    let fiexdOn = $(this).scrollTop()
+    if(fiexdOn > 0){
+      $("#header").addClass("fixedOn")
+    }else{
+      $("#header").removeClass("fixedOn")
+    }
+  })
+
+
   
 })
