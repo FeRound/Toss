0b4d7cb6ec4b4aede6a78429fef12de44de8d421
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-02 16:32:49 +0900

    d
    d

--- 토스/js/jquery.js
+++ 토스/js/jquery.js
@@ -1,14 +1,38 @@
 $(function(){
+  setTimeout(sectio1Event,500);// 섹션1 이벤트
+  
+  $(window).on("scroll",function(){
+    let wHeight = $(window).height();
+    let NowHeight = $(this).scrollTop()   
+    console.log(`현재스크롤높이:${NowHeight}`) 
 
-  $(window).on("scroll",function(){ // 헤더색상추가
-    let fiexdOn = $(this).scrollTop()
-    if(fiexdOn > 0){
+    if(NowHeight > 0){ //헤더 보더바텀 추가
       $("#header").addClass("fixedOn")
     }else{
       $("#header").removeClass("fixedOn")
     }
+
+    // && ($(this).offset().top) + wHeight > NowHeight
+    $("section").each(function(index){ // 특정 높이에 도달하면 active 클래스 추가
+      console.log(`${index + 1}번 섹션 : ${$(this).offset().top}`)
+      if(($(this).offset().top) < NowHeight){     
+        //이구간에 들어오면 특정시간후 active 속성을 차례대로 추가    
+        
+        console.log($(this).find("aniOn").addClass("active"));
+
+
+      }      
+    })
+
   })
-
-
-  
 })
+
+
+
+
+
+let sectio1Event = function(){ //섹션1 이벤트
+  $("#section1 .container").addClass("active")
+}
+
+
