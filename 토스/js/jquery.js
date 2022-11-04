7d49c6a2da89b7ea917844ffcf0f194924a0d35f
12312312441 <113410955+asd1231233@users.noreply.github.com>
2022-11-04 14:16:51 +0900

    ㅇ
    ㅇ

--- 토스/js/jquery.js
+++ 토스/js/jquery.js
@@ -1,10 +1,8 @@
 $(function(){  
-  setTimeout(headerEventOn,300) //헤더 실행 함수
   setTimeout(section1EventOn,700);//메인배너 실행 함수
-  $(window).scroll(windowScrollEvent) // 스크롤시 실행 함수
+  $(window).on("scroll",windowScrollEvent) // 스크롤시 실행 함수
+  $()
   HomeSliderOn() // 섹션5 슬라이드 함수
-  
-  console.log(percent)
 })
 
 function HomeSliderOn(){ 
@@ -18,20 +16,24 @@ function HomeSliderOn(){
     },4000); 
 }
 
-function headerEventOn(){
-  $("#header").addClass("active");
-}
+
 
 function section1EventOn(){
   $("#section1 .container").addClass("active")
 }
 
 function windowScrollEvent(){
-  let wHeight = $(window).height();
-  let NowHeight = $(this).scrollTop();
-  let percent = (NowHeight / ($(document).height() - $(window).height())) * 100; //전체 스크롤 퍼센트
+  //(NowHeight / ($(document).height() - $(window).height())) * 100; //전체 스크롤 퍼센트
+  let wHeight = $(window).height(); // 웹 높이
+  let NowHeight = $(this).scrollTop(); //현재 스크롤 높이
+  let section7Height = $("#section8").offset().top - $("#section7").offset().top // 섹션7 높이
+  let section7Offset = $("#section7").offset().top // 섹션7 위치
+  let section8Offset = $("#section8").offset().top // 섹션8 위치
+
   console.log(`현재스크롤높이:${NowHeight}`) 
-  console.log(`스크롤 : ${percent}`)
+ 
+  
+
 
   if(NowHeight > 0){ //헤더 보더바텀 추가
     $("#header").addClass("fixedOn")
@@ -39,21 +41,21 @@ function windowScrollEvent(){
     $("#header").removeClass("fixedOn")
   }
 
-  $("section").each(function(index){ // 특정 높이에 도달하면 active 클래스 추가
+  $("section").each(function(index,e){ // 특정 높이에 도달하면 active 클래스 추가
     console.log(`${index + 1}번 섹션 : ${$(this).offset().top}`)
     if(($(this).offset().top - 600) < NowHeight){     
       $(this).addClass("active");
-    }      
+    }  
   })
 
-  if(NowHeight > 8850 && NowHeight <= 9150){
-    let opacityOn = NowHeight - 8850;
-    $("chart-opacity1").css("opacity",`${opacityOn}`)
+
+
+  if(section7Offset <= NowHeight && section8Offset >= NowHeight){ // 섹션7 구역 안으로 스크롤 들어왔을때 작동
+    let percent = (NowHeight  / (section7Height - wHeight)) * 100;
+    console.log(percent);
   }
-  
-  if(percent > 49 && percent < 53){
-    $("#section8").css("width",`${percent}%`)
-  }
+
+
   
 
 }
